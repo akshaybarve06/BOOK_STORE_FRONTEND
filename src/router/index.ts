ba0338f1fa908e0fb2ac35ef5a80d925d788dbb0
sujitbidawe <bidawesujit@gmail.com>
2020-08-19 18:05:03 +0530

    ADDED: routing for signin, signup, forget password and reset password

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,36 +1,50 @@
-import Vue from "vue";
-import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
-import New from "../components/NewFile.vue";
+import Vue from 'vue'
+import VueRouter, { RouteConfig } from 'vue-router'
+import SignIn from '../components/SignIn.vue'
+import SignUp from '../components/SignUp.vue'
+import ForgotPassword from '../components/ForgotPassword.vue'
+import ResetPassword from '../components/ResetPassword.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
+  // {
+  //   path: "/about",
+  //   name: "About",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ "../views/About.vue")
+  // },
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: '/signin',
+    name: 'SignIn',
+    component: SignIn
   },
   {
-    path: "/new",
-    name: "New",
-    component: New
+    path: '/signup',
+    name: 'SignUp',
+    component: SignUp
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: '/forgot-password',
+    name: 'ForgotPassword',
+    component: ForgotPassword,
+    children: [
+      {
+        path: '/reset-password',
+        name: 'ResetPassword',
+        component: ResetPassword
+      }
+    ]
   }
-];
+]
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes
-});
+})
 
-export default router;
+export default router
