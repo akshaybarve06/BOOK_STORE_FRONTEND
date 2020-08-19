de662744c950c1d8bea43dba631c7b885a4518a6
sujitbidawe <bidawesujit@gmail.com>
2020-08-19 18:02:02 +0530

    ADDED: UI for SignIn page

---
+++ src/components/SignIn.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="login-box">
+    <h1>Welcome to BookStore</h1>
+    <div class="input-wrapper">
+      <input class="field" type="email" placeholder="Email"/>
+      <input class="field" type="password" placeholder="Password" />
+    </div>
+    <div>
+      <button class="button">Login</button>
+    </div>
+    <div>
+      <a class="ref-link" style="color:white"
+        href="/signup"
+      >Create account instead!</a>
+      <a class="ref-link" style="color:white"
+        href="/forgot-password"
+      >forgot password?</a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import '../assets/styles/App.scss'
+
+export default Vue.extend({
+  name: 'SignIn'
+})
+</script>
