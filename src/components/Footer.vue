4d571e057ddfbc37db760749d194db3446db1ee4
Aarti98765 <aartiahireengit@gmail.com>
2020-08-20 15:56:39 +0530

    Added : Code of footer in bookstore application

---
+++ src/components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="footer">
+            <text is="x3d" class="footer-text-style" >
+              Copyright@2020, Bookstore Private Limited. All Rights Reserved
+            </text>
+</div>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Footer'
+})
+</script>
+
+<style>
+  .footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(29, 140, 204);
+  color: white;
+  height: 50px;
+  font-family: fontawesome;
+  bottom: 0;
+  size: inherit;
+  margin-top: 400px;
+  position: sticky;
+  box-shadow: teal
+  }
+  .footer-text-style {
+  display: flex;
+  flex-direction: row;
+  }
+</style>
